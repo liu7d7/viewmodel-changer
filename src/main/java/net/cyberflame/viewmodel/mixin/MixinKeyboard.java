9b4156d90a584e5f40b4dc86bef7b20384eef5a6
Aaron Lowe <cyberflameu@gmail.com>
2023-07-23 15:09:27 +1200

    okay actually reading the error helped
    Signed-off-by: Aaron Lowe <cyberflameu@gmail.com>

--- src/main/java/net/cyberflame/viewmodel/mixin/MixinKeyboard.java
+++ src/main/java/net/cyberflame/viewmodel/mixin/MixinKeyboard.java
@@ -10,10 +10,10 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Keyboard.class)
-class MixinKeyboard {
+public class MixinKeyboard {
 
     @Inject(at = @At("HEAD"), method = "onKey(JIIII)V", cancellable = true)
-    private static void onOnKey(long windowHandle, int keyCode, int scanCode, int action, int modifiers, CallbackInfo ci) {
+    private void onOnKey(long windowHandle, int keyCode, int scanCode, int action, int modifiers, CallbackInfo ci) {
         if (null != MinecraftClient.getInstance().world && GLFW.GLFW_PRESS == action && GLFW.GLFW_KEY_BACKSLASH == keyCode) {
             MinecraftClient.getInstance().setScreen(new ViewmodelScreen());
         }
